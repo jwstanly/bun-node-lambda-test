67fb41937ba7417dd0df0232bb48527470a2112d
jwstanly <jwstanly@yahoo.com>
2023-09-13 16:02:09 -0400

    cleanup benchmarks

--- lib/benchmarks/benchmark.ts
+++ lib/benchmarks/benchmark.ts
@@ -1,49 +1,46 @@
 import { runtimes, testNames, memorySizes } from '../bun-node-test-stack';
+import {
+  exportAllDataToJson,
+  exportTestAveragesToCsv,
+  recordResult,
+} from './exports';
 
 const NUM_RUNS = 5;
 const SERVER = 'https://lrvxob60b9.execute-api.us-east-1.amazonaws.com/prod/';
 
-const testRoutes: string[] = [];
-const results: Record<string, Record<string, Record<string, number[]>>> = {};
+async function runBenchmarks() {
+  const testRoutes: string[] = [];
+  const results: Record<string, Record<string, Record<string, number[]>>> = {};
 
-function recordResult(route: string, time: number) {
-  const [runtime, testName, memorySize] = route.split('/');
-
-  console.log(`Finished ${route} in ${time}ms`);
-
-  if (!results[testName]) {
-    results[testName] = {};
-  }
-  if (!results[testName][memorySize]) {
-    results[testName][memorySize] = {};
-  }
-  if (!results[testName][memorySize][runtime]) {
-    results[testName][memorySize][runtime] = [];
-  }
-  results[testName][memorySize][runtime].push(time);
-}
-
-for (const runtime of runtimes) {
-  for (const name of testNames) {
-    for (const memorySize of memorySizes) {
-      testRoutes.push(`${runtime}/${name}/${memorySize}`);
+  for (const runtime of runtimes) {
+    for (const name of testNames) {
+      for (const memorySize of memorySizes) {
+        testRoutes.push(`${runtime}/${name}/${memorySize}`);
+      }
     }
   }
+
+  const apiCallFuncs = testRoutes.map((route) => async () => {
+    const response = await fetch(`${SERVER}${route}`);
+    const time = await response.text().then(Number);
+
+    recordResult(results, route, time);
+  });
+
+  await Promise.all(
+    apiCallFuncs.map(async (apiCall) => {
+      for (let i = 0; i < NUM_RUNS; i++) {
+        await apiCall();
+      }
+    }),
+  );
+
+  console.log('Results:', JSON.stringify(results, null, 2));
+
+  exportAllDataToJson(results);
+  for (const testName in results) {
+    exportTestAveragesToCsv(testName, results[testName]);
+  }
 }
 
-const apiCalls = testRoutes.map((route) => async () => {
-  const response = await fetch(`${SERVER}${route}`);
-  const time = await response.text().then(Number);
-
-  recordResult(route, time);
-});
-
-const benchmarks = apiCalls.map(async (apiCall) => {
-  for (let i = 0; i < NUM_RUNS; i++) {
-    await apiCall();
-  }
-});
-
-Promise.all(benchmarks).then(() => {
-  console.log('Results:', JSON.stringify(results, null, 2));
-});
+runBenchmarks();
---
+++ lib/benchmarks/exports.ts
@@ -0,0 +1,63 @@
+import * as fs from 'fs';
+import { runtimes, testNames, memorySizes } from '../bun-node-test-stack';
+
+export function exportTestAveragesToCsv(
+  testName: string,
+  results: Record<string, Record<string, number[]>>,
+) {
+  const averages = {};
+  for (const memorySize in results) {
+    averages[memorySize] = {};
+    for (const runtime of Object.keys(results[memorySize])) {
+      const values = results[memorySize][runtime];
+      const sum = values.reduce((acc, value) => acc + value, 0);
+      averages[memorySize][runtime] = sum / values.length;
+    }
+  }
+
+  const csv = [[testName, ...Object.keys(averages)]];
+  for (const runtime of runtimes) {
+    const row = [runtime];
+    for (const memorySize in averages) {
+      row.push(averages[memorySize][runtime]);
+    }
+    csv.push(row);
+  }
+
+  const csvString = csv.map((row) => row.join(',')).join('\n');
+  fs.writeFileSync(getPath(`${Date.now()}-${testName}.csv`), csvString);
+}
+
+export function exportAllDataToJson(
+  results: Record<string, Record<string, Record<string, number[]>>>,
+) {
+  fs.writeFileSync(
+    getPath(`${Date.now()}.json`),
+    JSON.stringify(results, null, 2),
+  );
+}
+
+export function recordResult(
+  results: Record<string, Record<string, Record<string, number[]>>>,
+  route: string,
+  time: number,
+) {
+  const [runtime, testName, memorySize] = route.split('/');
+
+  console.log(`Finished ${route} in ${time}ms`);
+
+  if (!results[testName]) {
+    results[testName] = {};
+  }
+  if (!results[testName][memorySize]) {
+    results[testName][memorySize] = {};
+  }
+  if (!results[testName][memorySize][runtime]) {
+    results[testName][memorySize][runtime] = [];
+  }
+  results[testName][memorySize][runtime].push(time);
+}
+
+function getPath(fileName: string) {
+  return `results/${fileName}`;
+}
